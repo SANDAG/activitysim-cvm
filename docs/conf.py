83cebc979994123c46e141ea21b8912a53d4fce4
Jeff Newman <jeffnewman@camsys.com>
2022-03-08 15:43:28 -0600

    switch to pydata theme

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,6 @@
 import sys
 import os
 
-import sphinx_rtd_theme
-
 # -- Get Package Version --------------------------------------------------
 import activitysim
 print("package version: " + activitysim.__version__)
@@ -111,7 +109,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -119,7 +117,7 @@ html_theme = "sphinx_rtd_theme"
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
