ee751e94982e2df07ac072aa4b0e1c017b9d6272
jpn-- <jpn--@users.noreply.github.com>
2022-08-24 16:09:03 +0000

    deploy: 2a92be8f9ec0966e54a076edd1cb50431931bb65

--- sharrow-3z/_static/documentation_options.js
+++ sharrow-3z/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.1.dev298+g1d56952e',
+    VERSION: '0.1.dev1+g2a92be8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
