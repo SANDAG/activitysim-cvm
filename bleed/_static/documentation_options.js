b54e533c5f3da09fcfbb034fb80508adfbb9854d
jpn-- <jpn--@users.noreply.github.com>
2022-08-24 16:08:49 +0000

    deploy: 2a92be8f9ec0966e54a076edd1cb50431931bb65

--- bleed/_static/documentation_options.js
+++ bleed/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g1d56952',
+    VERSION: '0.1.dev1+g2a92be8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
