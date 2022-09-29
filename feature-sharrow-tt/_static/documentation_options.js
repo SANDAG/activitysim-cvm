b7efb5307769109379d73458fcdc142ef6ab32f1
jpn-- <jpn--@users.noreply.github.com>
2022-09-29 15:52:25 +0000

    deploy: 19fe95ef5801c2f88a03c0df2c0da3f5bfe744e9

--- feature-sharrow-tt/_static/documentation_options.js
+++ feature-sharrow-tt/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.0.dev417+ga58b691c',
+    VERSION: '1.2.0.dev419+g19fe95ef',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
