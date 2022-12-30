f880b02ddf5ed5a11743d04e2ae4501851617775
Jeff Newman <jeffnewman@camsys.com>
2022-12-30 10:11:08 -0600

    blacken

--- activitysim/abm/models/ldt_scheduling.py
+++ activitysim/abm/models/ldt_scheduling.py
@@ -3,29 +3,24 @@ import logging
 import numpy as np
 import pandas as pd
 
-from activitysim.core import (
-    config,
-    expressions,
-    inject,
-    logit,
-    pipeline,
-    simulate,
-    tracing,
-)
-from activitysim.core.util import assign_in_place, reindex
+from activitysim.core import config, inject, logit, pipeline, simulate, tracing
+from activitysim.core.util import assign_in_place
 
 from .ldt_pattern import LDT_PATTERN
 from .util import estimation
 
 logger = logging.getLogger(__name__)
 
+
 @inject.step()
-def ldt_scheduling(longdist_tours, persons_merged, land_use, network_los, chunk_size, trace_hh_id):
+def ldt_scheduling(
+    longdist_tours, persons_merged, land_use, network_los, chunk_size, trace_hh_id
+):
     """
     This model schedules the start/end times for actors are on a trip
-    in a given day (only those in the longdist_tours csv). This specifically estimates 
+    in a given day (only those in the longdist_tours csv). This specifically estimates
     the start hour for begin trips, the end hour for end trips, and both the start/hour
-    hour for complete trips. 
+    hour for complete trips.
 
     - *Configuration File*: `ldt_scheduling.yaml`
     - *Core Table*: `longdist_tours`
@@ -51,7 +46,7 @@ def ldt_scheduling(longdist_tours, persons_merged, land_use, network_los, chunk_
     ldt_tours = longdist_tours.to_frame()
     land_use = land_use.to_frame()
     logger.info("Running %s with %d tours" % (trace_label, ldt_tours.shape[0]))
-    
+
     # merging in the persons_merged data into ldt_tours for estimation
     persons_merged = persons_merged.to_frame()
     ldt_tours_merged = pd.merge(
@@ -66,9 +61,9 @@ def ldt_scheduling(longdist_tours, persons_merged, land_use, network_los, chunk_
     # read in settings for all estimation categories (start, end, complete)
     spec_categories = model_settings.get("SPEC_CATEGORIES", {})
     nest_spec = config.get_logit_model_settings(model_settings)  # MNL
-    
-    # create a dictionary to translate estimated complete tour choices to 
-    # start and end hours 
+
+    # create a dictionary to translate estimated complete tour choices to
+    # start and end hours
     # earliest start hour is 5, latest end hour is 23, minimum duration is 2
     # duration = end hour - start hour
     complete_tour_translation = {}
@@ -85,41 +80,56 @@ def ldt_scheduling(longdist_tours, persons_merged, land_use, network_los, chunk_
             j += 1
         i += 1
     complete_tour_translation = pd.Series(complete_tour_translation)
-    
+
     # map patterns in settings to their respective enums
     patterns = {
         "complete": LDT_PATTERN.COMPLETE,
         "begin": LDT_PATTERN.BEGIN,
         "end": LDT_PATTERN.END,
     }
-    
+
     # read in and create time/dist skims between TAZs and internal TAZs
     model_area_key = model_settings.get("MODEL_AREA_KEY", None)
     segment_key = model_settings.get("SEGMENT_KEY", None)
     time_key = model_settings.get("TIME_KEY", None)
     dist_key = model_settings.get("DIST_KEY", None)
-    
-    assert model_area_key != None and segment_key != None and time_key != None and dist_key != None
-    
-    times, dists = get_skims(network_los, land_use, dist_key, time_key, segment_key, model_area_key)
-    
-    # get the TAZs that don't have possible destinations for a 2 hour complete ldt--can't travel there/back and spend the min time in the destination
+
+    assert (
+        model_area_key is not None
+        and segment_key is not None
+        and time_key is not None
+        and dist_key is not None
+    )
+
+    times, dists = get_skims(
+        network_los, land_use, dist_key, time_key, segment_key, model_area_key
+    )
+
+    # get the TAZs that don't have possible destinations for a 2 hour complete ldt
+    # --can't travel there/back and spend the min time in the destination
     # merge them into the choosers dataframe to prevent these 2 hour complete ldt tours
     invalid_tazs = []
     for taz in times.index:
-        if land_use.loc[taz][model_area_key] == 1 and 2 * np.where(dists.loc[taz] >= 50, times.loc[taz], 9999).min() + constants["min_time_in_dest"] >= 119:
+        if (
+            land_use.loc[taz][model_area_key] == 1
+            and 2 * np.where(dists.loc[taz] >= 50, times.loc[taz], 9999).min()
+            + constants["min_time_in_dest"]
+            >= 119
+        ):
             invalid_tazs.append(taz)
-    ldt_tours_merged["no_2_hour_tour"] = ldt_tours_merged["home_zone_id"].isin(invalid_tazs)
-    
+    ldt_tours_merged["no_2_hour_tour"] = ldt_tours_merged["home_zone_id"].isin(
+        invalid_tazs
+    )
+
     # lists to append all results to
     starts_list = []
     ends_list = []
-    
-    # estimate schedules for households/persons separately 
+
+    # estimate schedules for households/persons separately
     for actor_type, tours_segment in ldt_tours_merged.groupby(actor_column_name):
         # the segment to estimate on
         choosers = tours_segment
-        
+
         logger.info(
             "ldt_scheduling actor_type '%s' (%s tours)"
             % (
@@ -127,7 +137,7 @@ def ldt_scheduling(longdist_tours, persons_merged, land_use, network_los, chunk_
                 len(choosers.index),
             )
         )
-        
+
         # if there are no choosers, set default to -1
         if choosers.empty:
             starts_list.append(
@@ -137,7 +147,7 @@ def ldt_scheduling(longdist_tours, persons_merged, land_use, network_los, chunk_
                 pd.Series(-1, index=tours_segment.index, name=end_colname).to_frame()
             )
             continue
-        
+
         # estimate schedules for begin/end/complete tours separately
         for category_settings in spec_categories:
             # see function documentation for category_num translation
@@ -148,7 +158,7 @@ def ldt_scheduling(longdist_tours, persons_merged, land_use, network_los, chunk_
             subset = choosers[
                 (choosers["ldt_pattern"] & LDT_PATTERN.COMPLETE) == category_num
             ]
-            if(len(subset) == 0):
+            if len(subset) == 0:
                 continue
             # name of the current tour pattern beign estimated
             category_name = category_settings["NAME"]
@@ -156,7 +166,9 @@ def ldt_scheduling(longdist_tours, persons_merged, land_use, network_los, chunk_
             # logic for complete tour pattern scheduling
             if category_num == LDT_PATTERN.COMPLETE:
                 # read in specification for complete tour pattern scheduling
-                model_spec = simulate.read_model_spec(file_name=category_settings["SPEC"])
+                model_spec = simulate.read_model_spec(
+                    file_name=category_settings["SPEC"]
+                )
                 coefficients_df = simulate.read_model_coefficients(category_settings)
                 model_spec = simulate.eval_coefficients(
                     model_spec, coefficients_df, estimator
@@ -165,7 +177,9 @@ def ldt_scheduling(longdist_tours, persons_merged, land_use, network_los, chunk_
                 nest_spec = config.get_logit_model_settings(model_settings)
 
                 if estimator:
-                    estimator.write_model_settings(model_settings, model_settings_file_name)
+                    estimator.write_model_settings(
+                        model_settings, model_settings_file_name
+                    )
                     estimator.write_spec(model_settings)
                     estimator.write_coefficients(coefficients_df, model_settings)
                     estimator.write_choosers(choosers)
@@ -178,26 +192,39 @@ def ldt_scheduling(longdist_tours, persons_merged, land_use, network_los, chunk_
                     locals_d=constants,
                     chunk_size=chunk_size,
                     trace_label=trace_label,
-                    trace_choice_name="ldt_scheduling_" + actor_type + "_" + category_name,
+                    trace_choice_name="ldt_scheduling_"
+                    + actor_type
+                    + "_"
+                    + category_name,
                     estimator=estimator,
                 )
 
                 if estimator:
                     estimator.write_choices(choices)
                     choices = estimator.get_survey_values(
-                        choices, "persons", "ldt_scheduling_" + actor_type + "_" + category_name
+                        choices,
+                        "persons",
+                        "ldt_scheduling_" + actor_type + "_" + category_name,
                     )
                     estimator.write_override_choices(choices)
                     estimator.end_estimation()
-                    
+
                 # get the start/end hours associated with the estimated values
                 starts_ends = complete_tour_translation.loc[choices.values]
-                
-                start_choices = pd.DataFrame(starts_ends.apply(lambda x: x[0]).values, index=subset.index, columns=[start_colname])
-                end_choices = pd.DataFrame(starts_ends.apply(lambda x: x[1]).values, index=subset.index, columns=[end_colname])
-                
+
+                start_choices = pd.DataFrame(
+                    starts_ends.apply(lambda x: x[0]).values,
+                    index=subset.index,
+                    columns=[start_colname],
+                )
+                end_choices = pd.DataFrame(
+                    starts_ends.apply(lambda x: x[1]).values,
+                    index=subset.index,
+                    columns=[end_colname],
+                )
+
                 starts_list.append(start_choices)
-                ends_list.append(end_choices)                
+                ends_list.append(end_choices)
             # logic for start/end pattern scheduling
             else:
                 # get the freuqencies for the specific category type
@@ -223,40 +250,44 @@ def ldt_scheduling(longdist_tours, persons_merged, land_use, network_los, chunk_
                     ends_list.append(choices)
                 else:
                     raise ValueError(f"BUG, bad category_num {category_num}")
-          
+
     # merge in start hours if there are any, default -1
-    if len(starts_list) != 0:   
-        starts_df = pd.concat(starts_list).reindex(ldt_tours_merged.index).fillna(
-            {start_colname: -1}, downcast="infer"
+    if len(starts_list) != 0:
+        starts_df = (
+            pd.concat(starts_list)
+            .reindex(ldt_tours_merged.index)
+            .fillna({start_colname: -1}, downcast="infer")
         )
         assign_in_place(ldt_tours, starts_df)
-    
+
     # merge in end hours if there are any, default -1
     if len(ends_list) != 0:
-        ends_df = pd.concat(ends_list).reindex(ldt_tours_merged.index).fillna(
-            {end_colname: -1}, downcast="infer"
+        ends_df = (
+            pd.concat(ends_list)
+            .reindex(ldt_tours_merged.index)
+            .fillna({end_colname: -1}, downcast="infer")
         )
         assign_in_place(ldt_tours, ends_df)
-        
+
     tracing.print_summary(
         "ldt_scheduling all tour type start times",
         ldt_tours[start_colname],
         value_counts=True,
     )
-        
+
     tracing.print_summary(
         "ldt_scheduling all tour type end times",
         ldt_tours[end_colname],
         value_counts=True,
     )
-    
+
     # update ldt_tours
     pipeline.replace_table("longdist_tours", ldt_tours)
-    
+
     # merge start/end hours into households/persons for use in the internal mode model
     households = pipeline.get_table("households")
     persons = pipeline.get_table("persons")
-    
+
     # why do we need to merge to households/persons to get internal stuff to run?
     households[start_colname] = starts_df.reindex(households.index).fillna(
         {start_colname: -1}, downcast="infer"
@@ -264,17 +295,17 @@ def ldt_scheduling(longdist_tours, persons_merged, land_use, network_los, chunk_
     households[end_colname] = ends_df.reindex(households.index).fillna(
         {end_colname: -1}, downcast="infer"
     )
-    
+
     persons[start_colname] = starts_df.reindex(persons.index).fillna(
         {start_colname: -1}, downcast="infer"
     )
     persons[end_colname] = ends_df.reindex(persons.index).fillna(
         {end_colname: -1}, downcast="infer"
     )
-    
+
     pipeline.replace_table("households", households)
     pipeline.replace_table("persons", persons)
- 
+
     if trace_hh_id:
         tracing.trace_df(
             ldt_tours,
@@ -283,20 +314,57 @@ def ldt_scheduling(longdist_tours, persons_merged, land_use, network_los, chunk_
             index_label="tour_id",
             warn_if_empty=True,
         )
-        
+
+
 def get_skims(network_los, land_use, dist_key, time_key, segment_key, model_area_key):
     """
-        This function returns the time and distance skims for 
+    This function returns the time and distance skims for
     """
-    skims = network_los.get_default_skim_dict().skim_data._skim_data
-    key_dict = network_los.get_default_skim_dict().skim_dim3
-    time_val = key_dict[time_key][segment_key]
-    time_skim = skims[time_val]
-    
-    dist_val = key_dict[dist_key][segment_key]
-    dist_skim = skims[dist_val] 
-    internal_tazs = land_use[land_use[model_area_key] == 1].index
-    print(internal_tazs)
-    print(time_skim[:, internal_tazs - 1])
+    from ...core.skim_dataset import SkimDataset
+    from ...core.skim_dictionary import SkimDict
 
-    return pd.DataFrame(time_skim[:, internal_tazs - 1], index=np.arange(1, time_skim.shape[0] + 1), columns=internal_tazs), pd.DataFrame(dist_skim[:, internal_tazs - 1], index=np.arange(1, dist_skim.shape[0] + 1), columns=internal_tazs)
+    skim_dict = network_los.get_default_skim_dict()
+    if isinstance(skim_dict, SkimDict):
+        skims = skim_dict.skim_data._skim_data
+        key_dict = network_los.get_default_skim_dict().skim_dim3
+        time_val = key_dict[time_key][segment_key]
+        time_skim = skims[time_val]
+
+        dist_val = key_dict[dist_key][segment_key]
+        dist_skim = skims[dist_val]
+        internal_tazs = land_use[land_use[model_area_key] == 1].index
+        print(internal_tazs)
+        print(time_skim[:, internal_tazs - 1])
+
+        return pd.DataFrame(
+            time_skim[:, internal_tazs - 1],
+            index=np.arange(1, time_skim.shape[0] + 1),
+            columns=internal_tazs,
+        ), pd.DataFrame(
+            dist_skim[:, internal_tazs - 1],
+            index=np.arange(1, dist_skim.shape[0] + 1),
+            columns=internal_tazs,
+        )
+    elif isinstance(skim_dict, SkimDataset):
+        skims = skim_dict.skim_data._skim_data
+        key_dict = network_los.get_default_skim_dict().skim_dim3
+        time_val = key_dict[time_key][segment_key]
+        time_skim = skims[time_val]
+
+        dist_val = key_dict[dist_key][segment_key]
+        dist_skim = skims[dist_val]
+        internal_tazs = land_use[land_use[model_area_key] == 1].index
+        print(internal_tazs)
+        print(time_skim[:, internal_tazs - 1])
+
+        return pd.DataFrame(
+            time_skim[:, internal_tazs - 1],
+            index=np.arange(1, time_skim.shape[0] + 1),
+            columns=internal_tazs,
+        ), pd.DataFrame(
+            dist_skim[:, internal_tazs - 1],
+            index=np.arange(1, dist_skim.shape[0] + 1),
+            columns=internal_tazs,
+        )
+    else:
+        raise NotImplementedError(type(skim_dict))
