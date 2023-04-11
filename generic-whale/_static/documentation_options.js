87ae92c8e49e906e65ac821fbb38c8c756a6d1ba
jpn-- <jpn--@users.noreply.github.com>
2023-04-11 14:49:06 +0000

    deploy: 59b7d01500b88087937643b4eb4176787db02b1e

--- generic-whale/_static/documentation_options.js
+++ generic-whale/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1.dev180+ge7e521e8',
+    VERSION: '1.2.1.dev183+g59b7d015',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
