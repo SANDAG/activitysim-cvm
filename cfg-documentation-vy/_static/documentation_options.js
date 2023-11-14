9b50e16c6b740f6cbe53bb4387c4a4e762db76b0
jpn-- <jpn--@users.noreply.github.com>
2023-11-14 00:58:07 +0000

    deploy: f357115ee8ac904326ca05f97e47cd6e0b2fbc45

--- cfg-documentation-vy/_static/documentation_options.js
+++ cfg-documentation-vy/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1.dev408+g2c0b7004',
+    VERSION: '1.2.1.dev413+gf357115e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
