3be198902ef235a4b24ea71f18ca0b4b694394c7
vivekyadav26 <vivekyadav26@users.noreply.github.com>
2023-11-27 18:40:45 +0000

    deploy: 939e637df23d108fc48591d1546089af3186839f

--- cfg-documentation-vy/_static/documentation_options.js
+++ cfg-documentation-vy/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1.dev433+g9cb94005',
+    VERSION: '1.2.1.dev442+g939e637d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
